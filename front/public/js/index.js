4be29b9505396125fca6d90ba1d57b3eb02df052
Saurabh Shukla <123800993+saurabhshukla31@users.noreply.github.com>
2024-08-24 00:40:08 +0530

    Add files via upload

--- front/public/js/index.js
+++ front/public/js/index.js
@@ -1,150 +1,125 @@
 let gameHasStarted = false;
 var board = null;
 var game = new Chess();
-var $status = $('#status');
-var $pgn = $('#pgn');
+var $status = $("#status");
+var $pgn = $("#pgn");
 let gameOver = false;
 
-var pieceUrls = {
-    'wQ': 'https://assets-themes.chess.com/image/ejgfv/150/wq.png',
-    'wR': 'https://assets-themes.chess.com/image/ejgfv/150/wr.png',
-    'wP': 'https://assets-themes.chess.com/image/ejgfv/150/wp.png',
-    'wN': 'https://assets-themes.chess.com/image/ejgfv/150/wn.png',
-    'wK': 'https://assets-themes.chess.com/image/ejgfv/150/wk.png',
-    'wB': 'https://assets-themes.chess.com/image/ejgfv/150/wb.png',
-    'bR': 'https://assets-themes.chess.com/image/ejgfv/150/br.png',
-    'bQ': 'https://assets-themes.chess.com/image/ejgfv/150/bq.png',
-    'bP': 'https://assets-themes.chess.com/image/ejgfv/150/bp.png',
-    'bN': 'https://assets-themes.chess.com/image/ejgfv/150/bn.png',
-    'bK': 'https://assets-themes.chess.com/image/ejgfv/150/bk.png'
-};
+function onDragStart(source, piece, position, orientation) {
+  // do not pick up pieces if the game is over
+  if (game.game_over()) return false;
+  if (!gameHasStarted) return false;
+  if (gameOver) return false;
 
-// Fallback URL
-var fallbackPieceUrl = 'https://assets-themes.chess.com/image/ejgfv/150/bb.png';
+  if (
+    (playerColor === "black" && piece.search(/^w/) !== -1) ||
+    (playerColor === "white" && piece.search(/^b/) !== -1)
+  ) {
+    return false;
+  }
 
-function getPieceThemeUrl(piece) {
-    return pieceUrls[piece] || fallbackPieceUrl;
+  // only pick up pieces for the side to move
+  if (
+    (game.turn() === "w" && piece.search(/^b/) !== -1) ||
+    (game.turn() === "b" && piece.search(/^w/) !== -1)
+  ) {
+    return false;
+  }
 }
 
-function onDragStart (source, piece, position, orientation) {
-    // do not pick up pieces if the game is over
-    if (game.game_over()) return false;
-    if (!gameHasStarted) return false;
-    if (gameOver) return false;
+function onDrop(source, target) {
+  let theMove = {
+    from: source,
+    to: target,
+    promotion: "q", // NOTE: always promote to a queen for simplicity
+  };
+  // see if the move is legal
+  var move = game.move(theMove);
 
-    if ((playerColor === 'black' && piece.search(/^w/) !== -1) || (playerColor === 'white' && piece.search(/^b/) !== -1)) {
-        return false;
-    }
+  // illegal move
+  if (move === null) return "snapback";
 
-    // only pick up pieces for the side to move
-    if ((game.turn() === 'w' && piece.search(/^b/) !== -1) || (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
-        return false;
-    }
+  socket.emit("move", theMove);
+
+  updateStatus();
 }
 
-function onDrop (source, target) {
-    let theMove = {
-        from: source,
-        to: target,
-        promotion: 'q' // NOTE: always promote to a queen for simplicity
-    };
-    // see if the move is legal
-    var move = game.move(theMove);
-
-    // illegal move
-    if (move === null) return 'snapback';
-
-    socket.emit('move', theMove);
-    updateStatus();
-}
-
-socket.on('newMove', function(move) {
-    game.move(move);
-    board.position(game.fen());
-    updateStatus();
+socket.on("newMove", function (move) {
+  game.move(move);
+  board.position(game.fen());
+  updateStatus();
 });
 
 // update the board position after the piece snap
 // for castling, en passant, pawn promotion
-function onSnapEnd () {
-    board.position(game.fen());
+function onSnapEnd() {
+  board.position(game.fen());
 }
 
-function updateStatus () {
-    var status = '';
+function updateStatus() {
+  var status = "";
 
-    var moveColor = 'White';
-    if (game.turn() === 'b') {
-        moveColor = 'Black';
+  var moveColor = "White";
+  if (game.turn() === "b") {
+    moveColor = "Black";
+  }
+
+  // checkmate?
+  if (game.in_checkmate()) {
+    status = "Game over, " + moveColor + " is in checkmate.";
+  }
+
+  // draw?
+  else if (game.in_draw()) {
+    status = "Game over, drawn position";
+  } else if (gameOver) {
+    status = "Opponent disconnected, you win!";
+  } else if (!gameHasStarted) {
+    status = "Waiting for black to join";
+  }
+
+  // game still on
+  else {
+    status = moveColor + " to move";
+
+    // check?
+    if (game.in_check()) {
+      status += ", " + moveColor + " is in check";
     }
+  }
 
-    // checkmate?
-    if (game.in_checkmate()) {
-        status = 'Game over, ' + moveColor + ' is in checkmate.';
-    }
-
-    // draw?
-    else if (game.in_draw()) {
-        status = 'Game over, drawn position';
-    }
-
-    else if (gameOver) {
-        status = 'Opponent disconnected, you win!';
-    }
-
-    else if (!gameHasStarted) {
-        status = 'Waiting for black to join';
-    }
-
-    // game still on
-    else {
-        status = moveColor + ' to move';
-
-        // check?
-        if (game.in_check()) {
-            status += ', ' + moveColor + ' is in check';
-        }
-    }
-
-    $status.html(status);
-    $pgn.html(game.pgn());
-}
-
-function pieceTheme(piece) {
-    return getPieceThemeUrl(piece);
+  $status.html(status);
+  $pgn.html(game.pgn());
 }
 
 var config = {
-    draggable: true,
-    position: 'start',
-    onDragStart: onDragStart,
-    onDrop: onDrop,
-    onSnapEnd: onSnapEnd,
-    pieceTheme: pieceTheme // Use the function to get the URL for each piece
+  draggable: true,
+  position: "start",
+  onDragStart: onDragStart,
+  onDrop: onDrop,
+  onSnapEnd: onSnapEnd,
+  pieceTheme: "/public/img/chesspieces/wikipedia/{piece}.png",
 };
-
-// Initialize the board
-board = Chessboard('myBoard', config);
-
-if (playerColor === 'black') {
-    board.flip();
+board = Chessboard("myBoard", config);
+if (playerColor == "black") {
+  board.flip();
 }
 
 updateStatus();
 
 var urlParams = new URLSearchParams(window.location.search);
-if (urlParams.get('code')) {
-    socket.emit('joinGame', {
-        code: urlParams.get('code')
-    });
+if (urlParams.get("code")) {
+  socket.emit("joinGame", {
+    code: urlParams.get("code"),
+  });
 }
 
-socket.on('startGame', function() {
-    gameHasStarted = true;
-    updateStatus();
+socket.on("startGame", function () {
+  gameHasStarted = true;
+  updateStatus();
 });
 
-socket.on('gameOverDisconnect', function() {
-    gameOver = true;
-    updateStatus();
+socket.on("gameOverDisconnect", function () {
+  gameOver = true;
+  updateStatus();
 });
